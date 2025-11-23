ac9d4a248aaa55f273b37fa2af09ff0044b52053
Magnus Alvestad <magnus.alvestad@gmail.com>
2025-11-23 23:57:35 +0100

    Added mkfiles to run.bat

--- run.bat
+++ run.bat
@@ -9,6 +9,7 @@ cd userapp
 cargo build --release --bin rogue
 cargo build --release --bin shell
 cargo build --release --bin fstest
+cargo build --release --bin mkfiles
 cd ..
 cd rogue
 call build.bat
@@ -24,6 +25,7 @@ rem Copy binaries
 copy target\riscv64gc-unknown-none-elf\release\shell kernel\shell.elf
 copy target\riscv64gc-unknown-none-elf\release\rogue kernel\rogue.elf
 copy target\riscv64gc-unknown-none-elf\release\fstest kernel\fstest.elf
+copy target\riscv64gc-unknown-none-elf\release\mkfiles kernel\mkfiles.elf
 
 rem Build C App (if compiler exists, otherwise assume hello.elf exists or skip)
 if exist c_hello\hello.elf copy c_hello\hello.elf kernel\hello.elf
