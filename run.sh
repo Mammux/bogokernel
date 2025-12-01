636d67653e7938e18852782e026a0302bb0c0a1a
Magnus Alvestad <magnus.alvestad@gmail.com>
2025-12-01 15:17:14 +0100

    Removed run.sh as it is not updated

--- run.sh
+++
@@ -1,8 +0,0 @@
-cargo build -p userapp --release
-cp target/riscv64gc-unknown-none-elf/release/userapp kernel/userapp.elf
-
-cargo build -p cat --release
-cp target/riscv64gc-unknown-none-elf/release/cat kernel/cat.elf
-
-cargo build -Z build-std=core,alloc --target riscv64gc-unknown-none-elf -p kernel
-cargo run --target riscv64gc-unknown-none-elf --bin kernel
